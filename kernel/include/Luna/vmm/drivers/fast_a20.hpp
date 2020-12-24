4503f2a497a2ed71667c13c34d96933298b055dc
Thomas Woertman <twoertman@gmail.com>
2020-12-25 00:15:54 +0100

    vm/fast_a20: Handle A20 gate more robustly

--- kernel/include/Luna/vmm/drivers/fast_a20.hpp
+++ kernel/include/Luna/vmm/drivers/fast_a20.hpp
@@ -15,14 +15,30 @@ namespace vm::fast_a20 {
             vm->pio_map[reg_b] = this;
         }
 
-        void pio_write(uint16_t port, uint32_t value, [[maybe_unused]] uint8_t size) {
-            print("fast_a20: Unhandled write to port {:#x}: {:#x}\n", port, value);
+        void pio_write(uint16_t port, uint32_t value, uint8_t size) {
+            ASSERT(size == 1);
+
+            if(port == reg_a) {
+                auto a20 = (value >> 1) & 1;
+                if(!a20)
+                    PANIC("Guest attempted to clear A20 line");
+
+                if(value != (1 << 1))
+                    print("fast_20: Unhandled write to Port A: {:#x}\n", value);
+            } else {
+                print("fast_a20: Unhandled write to port {:#x}: {:#x}\n", port, value);
+            }
         }
 
-        uint32_t pio_read(uint16_t port, [[maybe_unused]] uint8_t size) {
-            print("fast_a20: Unhandled read from port {:#x}\n", port);
+        uint32_t pio_read(uint16_t port, uint8_t size) {
+            ASSERT(size == 1);
+            if(port == reg_a)
+                return (1 << 1); // Port A20 is always enabled
+            else {
+                print("fast_a20: Unhandled read from port {:#x}\n", port);
 
-            return 0;
+                return 0;    
+            }
         }
     };
 } // namespace vm::fast_a20
