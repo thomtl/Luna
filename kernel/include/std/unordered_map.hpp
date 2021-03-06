487b477f5e3fa79110fffaf0a8179068f2fca562
Thomas Woertman <twoertman@gmail.com>
2021-03-07 00:46:24 +0100

    std/unordered_map: Actual hash_map

--- kernel/include/std/unordered_map.hpp
+++ kernel/include/std/unordered_map.hpp
@@ -3,6 +3,9 @@
 #include <std/functional.hpp>
 #include <std/utility.hpp>
 #include <std/vector.hpp>
+#include <std/linked_list.hpp>
+
+#include <Luna/misc/log.hpp>
 
 namespace std
 {
@@ -17,36 +20,85 @@ namespace std
         using reference = value_type&;
         using const_reference = const value_type&;
 
-        // TODO: Normal hashmap
+        using entry_type = std::pair<Key, T>;
+
+
+        unordered_map() {
+            _map.resize(bucket_size);
+        }
+        
         T& operator[](const Key& key) {
-            auto correct_hash = _hasher(key);
+            auto& list = _map[_hasher(key) % bucket_size];
 
-            for(auto& e : _map)
-                if(e.first == correct_hash)
-                    return e.second;
+            for(auto& entry : list)
+                if(entry.first == key)
+                    return entry.second;
 
-            auto& e = _map.push_back(entry_type{correct_hash, {}});
+            auto& e = list.push_back({key, {}});
             return e.second;
         }
 
         bool contains(const Key& key) {
-            auto hash = _hasher(key);
+            auto& list = _map[_hasher(key) % bucket_size];
 
-            for(auto& e : _map)
-                if(e.first == hash)
+            for(auto& entry : list)
+                if(entry.first == key)
                     return true;
-
             return false;
         }
 
-        auto begin() { return _map.begin(); }
-        const auto begin() const { return _map.begin(); }
-        auto end() { return _map.end(); }
-        const auto end() const { return _map.end(); }
+        struct Iterator {
+            using entry_type = std::pair<Key, T>;
+
+            Iterator(unordered_map* map, entry_type* entry): map{map}, entry{entry} {}
+            Iterator(unordered_map* map): map{map} {
+                list = &map->_map[bucket_i];
+
+                while(list->size() == 0 && bucket_i < bucket_size)
+                    list = &map->_map[++bucket_i];
+
+                entry = &((*list)[list_i]);
+            }
+
+            entry_type& operator*() { return *entry; }
+            bool operator!=(Iterator it) { return entry != it.entry; }
+
+            void operator++() {
+                list_i++;
+                if(list_i >= list->size()) {
+                    bucket_i++;
+                    if(bucket_i >= bucket_size) {
+                        entry = nullptr;
+                        return;
+                    }
+                    
+                    list_i = 0;
+                }
+
+                entry = &((*list)[list_i]);
+            }
+
+            private:
+            size_t bucket_i = 0, list_i = 0;
+            unordered_map* map;
+            std::vector<entry_type>* list;
+            entry_type* entry;
+        };
+
+        Iterator begin() { return Iterator{this}; }
+        const Iterator begin() const { return Iterator{this}; }
+        Iterator end() { return Iterator{this, nullptr}; }
+        const Iterator end() const { return Iterator{this, nullptr}; }
+
+        void clear() {
+            _map.clear();
+            _map.resize(bucket_size);
+        }
 
         private:
-        using entry_type = std::pair<size_t, T>;
-        std::vector<entry_type> _map;
+        static constexpr size_t bucket_size = 64;
+        
+        std::vector<std::vector<entry_type>> _map;
         hasher _hasher;
     };
 } // namespace std
