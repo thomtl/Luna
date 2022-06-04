9afbee9558459f67ce0cbd2a12cec7d1008b5636
Thomas Woertman <twoertman@gmail.com>
2022-06-04 15:38:05 +0200

    tty: Fix regression

--- kernel/source/drivers/gpu/tty.cpp
+++ kernel/source/drivers/gpu/tty.cpp
@@ -16,7 +16,7 @@ void tty::Writer::putc(const char c) {
     
     auto print = [&]() {
         auto fb = gpu.get_fb();
-        auto* line = fb.data() + y * font::height * screen_pitch + x * font::width;
+        auto* line = (uint32_t*)fb.data() + y * font::height * screen_pitch + x * font::width;
 
         auto dc = (c >= 32) ? c : 127;
         for(uint8_t i = 0; i < font::height; i++) {
@@ -43,12 +43,12 @@ void tty::Writer::putc(const char c) {
         auto fb = gpu.get_fb();
 
         for(size_t i = 1; i < screen_height; i++) {
-            auto* dst = fb.data() + ((i - 1) * screen_pitch * font::height);
-            auto* src = fb.data() + (i * screen_pitch * font::height);
+            auto* dst = (uint32_t*)fb.data() + ((i - 1) * screen_pitch * font::height);
+            auto* src = (uint32_t*)fb.data() + (i * screen_pitch * font::height);
             memcpy(dst, src, mode.pitch * font::height);
         }
 
-        memset(fb.data() + ((screen_height - 1) * screen_pitch * font::height), 0, mode.pitch * font::height);
+        memset((uint32_t*)fb.data() + ((screen_height - 1) * screen_pitch * font::height), 0, mode.pitch * font::height);
 
         x = 0;
         y = screen_height;
