a176428a0af02d88a532b27df9ad5c4d47249251
Thomas Woertman <twoertman@gmail.com>
2022-07-12 21:16:19 +0200

    gui/LogWindow: Scroll with Keyboard

--- kernel/include/Luna/gui/windows/log_window.hpp
+++ kernel/include/Luna/gui/windows/log_window.hpp
@@ -13,9 +13,9 @@ namespace gui {
             controls::TextBox{size_chars},
             controls::VStack {
                 controls::Button{read_image("A:/luna/assets/toparrow.bmp").value(), read_image("A:/luna/assets/toparrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.root->get<0>().seek(controls::TextBox::SeekMode::Start); self.update_scrollbar(); }, this},
-                controls::Button{read_image("A:/luna/assets/uparrow.bmp").value(), read_image("A:/luna/assets/uparrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.root->get<0>().seek(controls::TextBox::SeekMode::Diff, -1); self.update_scrollbar(); }, this},
+                controls::Button{read_image("A:/luna/assets/uparrow.bmp").value(), read_image("A:/luna/assets/uparrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.scroll(-1); }, this},
                 controls::ScrollBar{{16, size_chars.y * font::height - 4 * 16}, {137, 7, 52}},
-                controls::Button{read_image("A:/luna/assets/downarrow.bmp").value(), read_image("A:/luna/assets/downarrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.root->get<0>().seek(controls::TextBox::SeekMode::Diff, 1); self.update_scrollbar(); }, this},
+                controls::Button{read_image("A:/luna/assets/downarrow.bmp").value(), read_image("A:/luna/assets/downarrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.scroll(1); }, this},
                 controls::Button{read_image("A:/luna/assets/bottomarrow.bmp").value(), read_image("A:/luna/assets/bottomarrow_clicked.bmp").value(), [](void* userptr) { auto& self = *(LogWindow*)userptr; self.root->get<0>().seek(controls::TextBox::SeekMode::End); self.update_scrollbar(); }, this},
                 
             }
@@ -26,7 +26,26 @@ namespace gui {
         void putc(const char c) { root->get<0>().putc(c); }
         void flush() { root->get<0>().update(); update_scrollbar(); request_redraw(); }
 
+        void handle_keyboard_op(KeyOp op, KeyCodes code) override {
+            if(op == KeyOp::Unpress)
+                return;
+
+            using enum KeyCodes;
+            switch (code) {
+                case UpArrow: scroll(-1); break;
+                case DownArrow: scroll(1); break;
+                case PageUp: scroll(-25); break;
+                case PageDown: scroll(25); break;
+                default: break;
+            }
+        }
+
         private:
+        void scroll(int64_t delta) {
+            root->get<0>().seek(controls::TextBox::SeekMode::Diff, delta);
+            update_scrollbar();
+        }
+
         void update_scrollbar() {
             auto& textbox = root->get<0>();
             root->get<1>().get<2>().update(textbox.n_lines(), size_chars.y, textbox.current_line());
